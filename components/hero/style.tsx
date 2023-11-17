44eefcfbba7d91317190762ec8817d5b8f0255b2
abdullox0900 <abdullox0900@gmail.com>
2023-11-17 23:47:58 +0500

    fix: style file

--- components/hero/style.tsx
+++ components/hero/style.tsx
@@ -2,30 +2,19 @@ import styled from 'styled-components'
 
 export const HeroWrap = styled.div`
 	padding: 70px 0px;
-
-
-
 `
 
 export const HeroWrapTop = styled.div`
 	display: flex;
 	align-items: center;
 	position: relative;
-
-
 	margin: 50px 0;
 `
 
 export const HeroAvatar = styled.img`
 	width: 180px;
 	height: 180px;
-
-
-
 	margin-right: 30px;
-
-
-
 	border-radius: 50%;
 	object-fit: cover;
     /* box-shadow: 0 10px 30px 0px #cacaca; */
@@ -36,8 +25,6 @@ export const HeroTitle = styled.h1`
 	font-style: normal;
 	font-weight: 500;
 	font-size: 40px;
-
-
 	line-height: 60px;
 	color: #fff;
 `
@@ -49,34 +36,18 @@ export const HeroContentTitle = styled.h2`
 	font-style: normal;
 	font-weight: 400;
 	font-size: 26px;
-
-
-
 	color: #000;
 	line-height: 47px;
-
-
-
 	letter-spacing: 0.02em;
 	margin-bottom: 20px;
-
-
 `
 
 export const HeroContentInfo = styled.p`
 	width: 450px;
-
-
 	margin-bottom: 30px;
 	font-style: normal;
-
-
 	font-weight: 400;
-
-	
 	font-size: 18px;
-
-
 	line-height: normal;
 	color: #545454;
 `
